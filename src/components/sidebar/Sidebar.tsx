d29dadc4ef0b62fe6d4c9d222e0ccd2b104fbd48
xlanik <lanik.mato@gmail.com>
2023-03-01 18:32:46 +0100

    TP2022T16-99 comment deletion

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 import { SidebarData } from './SidebarData'
 
-import { useState } from 'react'
 
 const Sidebar: React.FunctionComponent = () => {
-    const [close, setClose] = useState(false)
     return (
         <>
 
@@ -25,47 +23,4 @@ const Sidebar: React.FunctionComponent = () => {
         </>
     )
 }
-
-//to={item.path} 
-/*
-const SidebarMenu = styled.div`
-    width: 80px;
-    height: 100vh;
-    background-color: #9EDCFF;
-    position: fixed;
-    top: 0;
-    transition: .6s;
-`
-
-const MenuItems = styled.li`
-    list-style: none;
-    display: flex;
-    align-items: center;
-    justify-content: start;
-    width: 100%;
-    height: 90px;
-    padding: 1rem 0 1.25rem;
-`
-
-const MenuItemLinks = styled.div`
-    display: flex;
-    align-items: center;
-    padding: 0 2rem;
-    font-size: 20px;
-    text-decoration: none;
-    margin-bottom: ${props => props.to === "/" ? "1.5em" : ""};
-    color: #ffffff;
-    
-
-    &:hover {
-        background-color: #ffffff;
-        color: #000080;
-        width: 100%;
-        height: 45px;
-        width:
-        text-align: center;
-        border-radius: 5px;
-    }
-`
-*/
 export default Sidebar
