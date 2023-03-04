c71e274093fb57525e1be5bbde89f2c7e535749a
xlanik <lanik.mato@gmail.com>
2023-03-04 15:37:45 +0100

    TP2022T16-99 Formatting fix

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,26 +1,57 @@
 import React from 'react'
 
-import { SidebarData } from './SidebarData'
+import * as FaIcons from 'react-icons/fa'
 
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <FaIcons.FaHome />,
+  },
+  {
+    path: '/tasks',
+    icon: <FaIcons.FaSearch />,
+  },
+  {
+    path: '/pen',
+    icon: <FaIcons.FaPen />,
+  },
+  {
+    path: '/citations',
+    icon: <FaIcons.FaFileExport />,
+  },
+  {
+    path: '/share',
+    icon: <FaIcons.FaShareAlt />,
+  },
+  {
+    path: '/info',
+    icon: <FaIcons.FaInfoCircle />,
+  },
+  {
+    path: '/download',
+    icon: <FaIcons.FaDownload />,
+  },
+]
 
 const Sidebar: React.FunctionComponent = () => {
-    return (
-        <>
-
-            <div className="w-15 h-screen bg-blue-200 fixed top-0 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">
-
-                {SidebarData.map((item, index) => {
-                    return (
-                        <div className={"list-none flex items-center w-full h-90 px-2 py-5 "}>
-                            <button id={item.path} className={`px-5 py-2 text-lg text-white bg-blue-200 rounded-md hover:bg-blue-500 ${item.path === '/' ? 'mb-16' : 'mb-2'}`}>
-                                {item.icon}
-                            </button>
-
-                        </div>
-                    )
-                })}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="w-15 h-screen bg-blue-200 fixed top-0 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">
+        {SidebarData.map((item) => (
+          <div className={'list-none flex items-center w-full h-90 px-2 py-5 '}>
+            <button
+              id={item.path}
+              className={`px-5 py-2 text-lg text-white bg-blue-200 rounded-md hover:bg-blue-500 ${
+                item.path === '/' ? 'mb-16' : 'mb-2'
+              }`}
+            >
+              {item.icon}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
 export default Sidebar
