97f6a2a1d6cfedb87d3dadb8c5df0d8fa4ca55ca
xlanik <lanik.mato@gmail.com>
2023-04-01 18:42:45 +0200

    merge conflict fix

--- src/lib/components/sidebar/Sidebar.tsx
+++ src/lib/components/sidebar/Sidebar.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useState } from 'react'
-// import * as FaIcons from 'react-icons/fa'
 import { SIDEBAR_TABS } from '../../../utils/enums'
 
 import Home from './Home'
@@ -19,73 +18,45 @@ const Sidebar: React.FunctionComponent = () => {
   const SidebarData = [
     {
       name: 'home',
-<<<<<<< HEAD:src/components/sidebar/Sidebar.tsx
       icon: 'src/utils/images/home.svg',
-=======
-      // icon: <FaIcons.FaHome />,
       tooltip: 'Home',
->>>>>>> 76fc9b131d3d31b06d00b0d73e622cf3c5621bb2:src/lib/components/sidebar/Sidebar.tsx
       onClick: () => setActiveSidebar(SIDEBAR_TABS.HOME),
     },
     {
       name: 'search',
-<<<<<<< HEAD:src/components/sidebar/Sidebar.tsx
       icon: 'src/utils/images/search.svg',
-=======
-      // icon: <FaIcons.FaSearch />,
       tooltip: 'Full text search',
->>>>>>> 76fc9b131d3d31b06d00b0d73e622cf3c5621bb2:src/lib/components/sidebar/Sidebar.tsx
       onClick: () => setActiveSidebar(SIDEBAR_TABS.SEARCH),
     },
     {
       name: 'pen',
-<<<<<<< HEAD:src/components/sidebar/Sidebar.tsx
       icon: 'src/utils/images/pen.svg',
-=======
-      // icon: <FaIcons.FaPen />,
       tooltip: 'Document editing',
->>>>>>> 76fc9b131d3d31b06d00b0d73e622cf3c5621bb2:src/lib/components/sidebar/Sidebar.tsx
       onClick: () => setActiveSidebar(SIDEBAR_TABS.PEN),
     },
     {
       name: 'citations',
-<<<<<<< HEAD:src/components/sidebar/Sidebar.tsx
       icon: 'src/utils/images/citations.svg',
-=======
-      // icon: <FaIcons.FaFileExport />,
       tooltip: 'Generate citations',
->>>>>>> 76fc9b131d3d31b06d00b0d73e622cf3c5621bb2:src/lib/components/sidebar/Sidebar.tsx
       onClick: () => setActiveSidebar(SIDEBAR_TABS.CITATIONS),
     },
     {
       name: 'share',
-<<<<<<< HEAD:src/components/sidebar/Sidebar.tsx
       icon: 'src/utils/images/share.svg',
-=======
-      // icon: <FaIcons.FaShareAlt />,
       tooltip: 'Share document',
->>>>>>> 76fc9b131d3d31b06d00b0d73e622cf3c5621bb2:src/lib/components/sidebar/Sidebar.tsx
       onClick: () => setActiveSidebar(SIDEBAR_TABS.SHARE),
     },
     {
       name: 'info',
-<<<<<<< HEAD:src/components/sidebar/Sidebar.tsx
       icon: 'src/utils/images/info.svg',
-=======
-      // icon: <FaIcons.FaInfoCircle />,
       tooltip: 'Document information',
->>>>>>> 76fc9b131d3d31b06d00b0d73e622cf3c5621bb2:src/lib/components/sidebar/Sidebar.tsx
       onClick: () => setActiveSidebar(SIDEBAR_TABS.INFO),
     },
     {
       name: 'download',
-<<<<<<< HEAD:src/components/sidebar/Sidebar.tsx
       icon: 'src/utils/images/download.svg',
 
-=======
-      // icon: <FaIcons.FaDownload />,
       tooltip: 'Download document',
->>>>>>> 76fc9b131d3d31b06d00b0d73e622cf3c5621bb2:src/lib/components/sidebar/Sidebar.tsx
       onClick: () => setActiveSidebar(SIDEBAR_TABS.DOWNLOAD),
     },
   ]
@@ -106,12 +77,7 @@ const Sidebar: React.FunctionComponent = () => {
               }`}
               title={item.tooltip}
             >
-<<<<<<< HEAD:src/components/sidebar/Sidebar.tsx
               <img src={item.icon} className="w-6 h-6" />
-=======
-              {/* @ts-ignore */}
-              {item.icon}
->>>>>>> 76fc9b131d3d31b06d00b0d73e622cf3c5621bb2:src/lib/components/sidebar/Sidebar.tsx
             </button>
           </div>
         ))}
