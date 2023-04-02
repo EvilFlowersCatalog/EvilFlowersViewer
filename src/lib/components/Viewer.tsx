1e637eb0527be6717e25a33f092ed4046955dbf1
Daniel Cok <Daniel.Cok@onsemi.com>
2023-04-02 22:54:44 +0200

    IViewerProps Spread, usinf config instead of specific props

--- src/lib/components/Viewer.tsx
+++ src/lib/components/Viewer.tsx
@@ -9,21 +9,40 @@ pdfjs.GlobalWorkerOptions.workerSrc = PDFJSWorker
 
 interface IViewerProps {
   data?: string
+  buttons?: {
+    search?: boolean
+    edit?: boolean
+    message?: boolean
+    share?: boolean
+    info?: boolean
+    download?: boolean
+  }
+  flags?: {
+    showAnnotations?: boolean
+    showComments?: boolean
+    showLeftPanel?: boolean
+    showMessage?: boolean
+    showPrint?: boolean
+    showProperties?: boolean
+  }
 }
 
-export const Viewer = ({ data }: IViewerProps) => {
+export const Viewer = ( config : IViewerProps ) => {
   const [documentData, setDocumentData] = useState<string>()
 
   // On every data change, convert it to binary and set it to the documentData state
   useEffect(() => {
-    if (!data) return
-    const binary = base64ToBinary(data)
+    if (!config.data) return
+    const binary = base64ToBinary(config.data)
 
     setDocumentData(binary)
-  }, [data])
+  }, [config.data])
 
   return (
-    <div id="evilFlowersViewer" className="evilFlowersViewer bg-gray-100 w-screen h-screen">
+    <div
+      id="evilFlowersViewer"
+      className="evilFlowersViewer bg-gray-100 w-screen h-screen"
+    >
       {documentData && <Document data={documentData} />}
     </div>
   )
