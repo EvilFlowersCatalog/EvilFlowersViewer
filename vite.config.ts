356b8788cbe61d29f7e97376bf5b58de6e5a4302
rastiQ <rasto.balcercik@gmail.com>
2023-04-09 15:12:45 +0200

    staging bundle

--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,59 @@ import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import tailwindcss from 'tailwindcss'
 import svgr from 'vite-plugin-svgr'
+import { resolve } from 'path'
+import { UserConfig } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-    svgr()
-  ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss],
-    },
-  },
-  build: {
-    lib: {
-      entry: './src/lib/index.ts',
-      name: 'evilFlowersViewer',
-      formats: ['es', 'umd'],
-      fileName: (format) => `evilFlowersViewer.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+export default defineConfig(({ mode }: UserConfig) => {
+  if (mode === 'staging') {
+    return {
+      plugins: [
+        react(),
+        dts({
+          insertTypesEntry: true,
+        }),
+        svgr(),
+      ],
+      css: {
+        postcss: {
+          plugins: [tailwindcss],
         },
       },
-    },
-  },
+    }
+  }
+  if (mode === 'production') {
+    return {
+      plugins: [
+        react(),
+        dts({
+          insertTypesEntry: true,
+        }),
+        svgr(),
+      ],
+      css: {
+        postcss: {
+          plugins: [tailwindcss],
+        },
+      },
+      build: {
+        lib: {
+          entry: './src/lib/index.ts',
+          name: 'evilFlowersViewer',
+          formats: ['es', 'umd'],
+          fileName: (format) => `evilFlowersViewer.${format}.js`,
+        },
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+            },
+          },
+        },
+      },
+    }
+  }
+
+  return {}
 })
