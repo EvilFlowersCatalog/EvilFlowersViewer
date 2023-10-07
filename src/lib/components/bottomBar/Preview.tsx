75d5535ef0fe35c04a265ca4a72dad0acaac01e2
Jaroslav Drozd <jaroslavdrozd@Jaroslavs-MacBook-Pro.local>
2023-10-07 11:41:04 +0200

    added comments + fixed position for autoscroll

--- src/lib/components/bottomBar/Preview.tsx
+++ src/lib/components/bottomBar/Preview.tsx
@@ -15,15 +15,19 @@ const Preview = () => {
     setPage,
   } = useDocumentContext()
 
+  // use call back func to generate one page
   const renderPage = useCallback(
     async (givenPage: number, canvas: HTMLCanvasElement, div: HTMLElement) => {
       await new Promise((resolve) => {
+        // get page
         pdf?.getPage(givenPage).then(async (page) => {
+          // create desired scale
           let viewport = page.getViewport({ scale: 1 })
           const desiredHeight = 130
           const desiredScale = desiredHeight / viewport.height
           viewport = page.getViewport({ scale: desiredScale })
 
+          // create canvas
           canvas.height = viewport.height
           canvas.width = viewport.width
           const context = canvas.getContext('2d')
@@ -32,9 +36,10 @@ const Preview = () => {
             viewport: viewport,
           })
 
+          // redner and replace everything in div with created canvas
           await renderTask.promise.then(() => {
             div.replaceChildren(canvas)
-            resolve('')
+            resolve('') // return
           })
         })
       })
@@ -43,25 +48,31 @@ const Preview = () => {
   )
 
   useEffect(() => {
-    positions = []
+    positions = [] // reset
     setPreviewRender(RENDERING_STATES.RENDERING)
+
+    // create loader (spinning wheel)
     const loader = document.createElement('div')
     loader.setAttribute('class', 'viewer-loader-small')
 
+    // func for creating pages
     const startRender = async () => {
-      const previewBar = document.getElementById('previewBarContainer')!
+      const previewBar = document.getElementById('previewBarContainer')! // get container
 
+      // for each page
       for (let page = 1; page <= totalPages; page++) {
-        setPreviewRender(RENDERING_STATES.RENDERING)
+        // create canvas
         const canvas = document.createElement('canvas')
-        canvas.setAttribute('class', 'preview-bar-page')
+        canvas.setAttribute('class', 'preview-bar-page') // give it style
         if (page === activePage) {
-          canvas.classList.add('preview-bar-active-page')
+          canvas.classList.add('preview-bar-active-page') // if it's active
         }
 
+        // take div
         let div: HTMLElement | null = document.getElementById(
           'previewPage' + page
         )
+        // if it does not exist, create one
         if (!div) {
           div = document.createElement('div')
           div.setAttribute('id', 'previewPage' + page)
@@ -69,20 +80,22 @@ const Preview = () => {
           div.onclick = () => {
             setPage(page)
           }
-          div.appendChild(loader)
-          previewBar.appendChild(div)
+          div.appendChild(loader) // append loader
+          previewBar.appendChild(div) // append to container
         } else {
-          div.replaceChildren()
-          div.appendChild(loader)
+          div.replaceChildren() // delete everything
+          div.appendChild(loader) // append loader
         }
 
-        const divBounding = div.getBoundingClientRect()
-        positions.push({ x: divBounding.x, width: divBounding.width })
-
-        await renderPage(page, canvas, div)
+        // when page renders get psitions of the div
+        await renderPage(page, canvas, div).then(() => {
+          const divBounding = div!.getBoundingClientRect()
+          positions.push({ x: divBounding.x, width: divBounding.width })
+        })
       }
     }
 
+    // when everythings rendered set to RENDERED
     startRender().then(() => {
       setPreviewRender(RENDERING_STATES.RENDERED)
     })
@@ -90,7 +103,7 @@ const Preview = () => {
 
   useEffect(() => {
     if (previewRender === RENDERING_STATES.RENDERED) {
-      // update onclick function
+      // update onclick function each time active page change
       for (let page = 1; page <= totalPages; page++) {
         let div: HTMLElement | null = document.getElementById(
           'previewPage' + page
@@ -105,34 +118,33 @@ const Preview = () => {
       // updated active one
       const prevActive = document.getElementById('previewPage' + prevActivePage)
       const newActive = document.getElementById('previewPage' + activePage)
+
+      // change active styles
       if (prevActive) {
         const canvas = prevActive.querySelector('canvas')
         if (canvas) canvas.classList.remove('preview-bar-active-page')
       }
       if (newActive) {
-        // updated active one
-        const prevActive = document.getElementById(
-          'previewPage' + prevActivePage
-        )
-        const newActive = document.getElementById('previewPage' + activePage)
-        if (prevActive) {
-          const canvas = prevActive.querySelector('canvas')
-          if (canvas) canvas.classList.remove('preview-bar-active-page')
-        }
-        if (newActive) {
-          const canvas = newActive.querySelector('canvas')
-          if (canvas) canvas.classList.add('preview-bar-active-page')
-          const container = document.getElementById('previewBar')
+        const canvas = newActive.querySelector('canvas')
+        if (canvas) canvas.classList.add('preview-bar-active-page')
+        const container = document.getElementById('previewBar')
 
-          // scroll
-          if (container && canvas) {
-            const containerRect = container.getBoundingClientRect()
+        // auto scroll
+        if (container) {
+          const containerRect = container.getBoundingClientRect()
+
+          if (positions.length > 0) {
+            // update width of active one (cut of the border in active one)
+            const temp = positions[prevActivePage - 1].width
+            positions[prevActivePage - 1].width =
+              positions[activePage - 1].width
+            positions[activePage - 1].width = temp
 
             // Calculate the target scroll position to center the picture in the container
             const targetScrollPosition =
               positions[activePage - 1].x -
               containerRect.x -
-              (containerRect.width - positions[activePage - 1].width) / 2 // Center the picture
+              (containerRect.width - positions[activePage - 1].width) / 2
 
             // Scroll to the target position smoothly
             container.scrollTo({
