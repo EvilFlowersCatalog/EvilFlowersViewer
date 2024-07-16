28cc8a18a6fa977c3e5f59685468fdcf03b4099e
Jaroslav Drozd <jaroslavdrozd@Jaroslavs-MacBook-Pro.local>
2024-07-16 16:48:52 +0200

    set up new workerSrc

--- src/lib/components/Viewer.tsx
+++ src/lib/components/Viewer.tsx
@@ -7,8 +7,9 @@ import { ImExit } from 'react-icons/im'
 import { TypedArray } from 'pdfjs-dist/types/src/display/api'
 import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf.js'
 import { ViewerContext } from './hooks/useViewerContext'
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-  '../../../node_modules/pdfjs-dist/legacy/build/pdf.worker.js'
+pdfjsLib.GlobalWorkerOptions.workerSrc = import.meta.env.PROD
+  ? 'pdfjs-dist/legacy/build/pdf.worker.js'
+  : '../../../node_modules/pdfjs-dist/legacy/build/pdf.worker.js'
 
 export interface IViewerOptions {
   theme?: 'dark' | 'light'
